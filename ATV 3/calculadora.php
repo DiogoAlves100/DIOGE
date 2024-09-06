f845fd75667c5e8860b7a14e5b51d3fbe8e1ee85
Diogo Alves <diogobandeira389@gmail.com>
2024-09-06 20:56:19 -0300

    atv
    2

--- ATV 3/calculadora.php
+++ ATV 3/calculadora.php
@@ -1,35 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>calculadora</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <?php
-    
-$n1=$_GET["n1"];
-$n2=$_GET["n2"];
-$operação=$_GET["operação"];
+    <main>
+<?php
+$n1 = 0;
+$n2 = 0;
+$resultado = 0;
 
-$calculadora->setNumero1($n1);
-$calculadora->setNumero2($n2);
-switch($operacao){
+$n1 = $_GET["n1"];
+$n2 = $_GET["n2"];
+$calculadora = $_GET["calculadora"];
+
+switch ($calculadora) {
     case "somar":
-        $calculadora->somar();
+        $resultado = $n1 + $n2;
+        echo "Resultado: $resultado";
         break;
     case "subtrair":
-        $calculadora->subtrair();
-        break;
-    case "dividir":
-        $calculadora->dividir();
+        $resultado = $n1 - $n2;
+        echo "Resultado: $resultado";
         break;
     case "multiplicar":
-        $calculadora->multiplicar();
+        $resultado = $n1 * $n2;
+        echo "Resultado: $resultado";
+        break;
+    case "dividir":
+        $resultado = $n1 / $n2;
+        echo "Resultado: $resultado";
         break;
 }
-
-
 ?>
+</main>
 </body>
+
 </html>
